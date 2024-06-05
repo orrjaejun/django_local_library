02986ad694e712e7bf02d2a42d17873972499acf
JJ\jjorr <jjorr123@gmail.com>
2024-06-05 20:17:28 +0900

    register page update

---
+++ locallibrary/templates/registration/templatetags/form_tags.py
@@ -0,0 +1,9 @@
+# your_app/templatetags/form_tags.py
+
+from django import template
+
+register = template.Library()
+
+@register.filter(name='add_class')
+def add_class(value, arg):
+    return value.as_widget(attrs={'class': arg})
